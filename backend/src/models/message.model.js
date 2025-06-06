fff4112127310e99d3d9672b690f74e14b9c0d3c
HimanshuSharma141 <bagrahimanshu7@gmail.com>
2025-06-07 02:53:44 +0530

    made the models for the message section

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+    {
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,   // Reference to the User model
+            ref: "User", // Assuming you have a User model
+            required: true, 
+        },
+        reciverId : {
+            type: mongoose.Schema.Types.ObjectId,  // Reference to the User model
+            ref: "User", // Assuming you have a User model
+            required: true,
+        },
+        text: {
+            type: String,
+        },
+        image: {
+            type: String,
+            
+        }, 
+    }, 
+    {timestamps: true }
+ );
+
+ const message = mongoose.model("message", userSchema); // y wali line isliye likhi h taki apn isko aage bhi use kar sake 
+
+ export default message;
