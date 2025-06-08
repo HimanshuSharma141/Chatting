4611dea1413de56ef31d7602c0489d53167a150c
HimanshuSharma141 <bagrahimanshu7@gmail.com>
2025-06-09 04:05:43 +0530

    feat: integrate theme management and update routing logic in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,23 @@ import ProfilePage from './Pages/ProfilePage'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useUserAuthStore } from './store/userAuthStore'
+import { useThemeStore } from './store/useThemeStore'
 import { Loader } from 'lucide-react'
 import { Toaster } from 'react-hot-toast'
 
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useUserAuthStore();
-  
+  const { theme } = useThemeStore();
     useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  useEffect(() => {
+    if (theme) {
+      document.documentElement.setAttribute('data-theme', theme);
+    }
+  }, [theme]);
 
   if (isCheckingAuth && !authUser)
     return (
@@ -26,14 +33,14 @@ const App = () => {
       </div>
     );
   return (
-    <div>
+    <div> 
       <Navbar />
       <Routes>
-        <Route path="/" element={authUser ?  <HomePage /> : <Navigate to='/signup' />} />
+        <Route path="/" element={authUser ?  <HomePage /> : <Navigate to='/login' />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
         <Route path="/login" element={!authUser ? <LoginPage />  : <Navigate to='/' />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />     
         </Routes>
         <Toaster />
 
