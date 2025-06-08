d74eff08860e17c64e27cda077725daf5e1ecf0f
HimanshuSharma141 <bagrahimanshu7@gmail.com>
2025-06-08 12:11:10 +0530

    fix: correct typo in generateToken function call in login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    generateToken(user._id, res);
+    genrateToken(user._id, res);
 
     res.status(200).json({
       _id: user._id,
