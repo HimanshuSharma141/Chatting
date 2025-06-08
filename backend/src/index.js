3907dcd4eef3c72b29e904c81eec97b1968457b4
HimanshuSharma141 <bagrahimanshu7@gmail.com>
2025-06-08 12:11:01 +0530

    feat: add CORS middleware to enable cross-origin requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'; // Middleware to parse cookies
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import messageRoutes from './routes/message.route.js'; // Importing message routes if you have them defined
-
+import cors from 'cors'; // Importing CORS middleware to handle cross-origin requests
 
 // Importing the auth routes from the auth.route.js file
 dotenv.config(); // Load environment variables from .env file 
@@ -15,12 +15,16 @@ const app = express();
 
 const PORT =  process.env.PORT;
 
+app.use(cors({
+  origin: 'http://localhost:5173', // Change this to your frontend URL
+  credentials: true // Allow credentials (cookies, authorization headers, etc.)
+})); // Middleware to enable CORS for the specified origin  
+
 app.use(express.json()); // Middleware to parse JSON bodies simply can extract data from the request body
 app.use(cookieParser()); // Middleware to parse cookies from the request headers
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages", messageRoutes); // Assuming you have message routes defined
-
 
 app.listen (PORT, () => { 
   console.log('Server is running on port:' + PORT);
