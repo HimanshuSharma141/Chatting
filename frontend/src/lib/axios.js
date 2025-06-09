111bc829e9c2c4c41f33585b466de3a843dd1596
HimanshuSharma141 <bagrahimanshu7@gmail.com>
2025-06-10 04:10:11 +0530

    feat: update axios instance to dynamically set base URL based on environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',   // Change this to your backend URL
+    // Set the base URL for Axios requests.
+    // If the application is running in development mode, use the local backend URL.
+    // Otherwise, use the production backend URL relative to the current domain.
+    baseURL: import.meta.env.MODE === "development" ? 'http://localhost:5001/api': "/api",
     withCredentials: true, // This is important for sending cookies with requests
 });
